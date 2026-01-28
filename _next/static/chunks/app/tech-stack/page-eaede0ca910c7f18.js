07edbb7e579e45b4ce6a5df5f730502c46c4dec0
vishwash03 <vishwashtak9479@gmail.com>
2026-01-29 01:06:22 +0530

    Add files via upload

---
+++ _next/static/chunks/app/tech-stack/page-eaede0ca910c7f18.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[874],{8791:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8173,23)),Promise.resolve().then(n.t.bind(n,7970,23)),Promise.resolve().then(n.bind(n,9626)),Promise.resolve().then(n.bind(n,5239)),Promise.resolve().then(n.bind(n,6036))}},e=>{var s=s=>e(e.s=s);e.O(0,[479,500,997,592,441,517,358],()=>s(8791)),_N_E=e.O()}]);
